1dbaa7cbc9f0a8d3061c325128d5e939bb8c4c75
outranker <outranker222@gmail.com>
2022-12-19 21:18:33 +0900

    update

--- src/components/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
-import styled from "styled-components";
-
-const Layout = () => {
-  return <div>Layout</div>;
-};
-
-export default Layout;
---
+++ src/components/Logo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Logo = () => {
+  return <div>Logo</div>;
+};
+
+export default Logo;
